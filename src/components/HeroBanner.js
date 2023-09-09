480920f08c3b7bca7a47a548f58529859ad35a95
ephraim69 <ephraimigbinosa@gmail.com>
2023-09-09 11:11:46 +0100

    update hero banner

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -1,8 +1,31 @@
-import React from 'react'
+import React from 'react';
+import { Box, Stack, Typography, Button } from '@mui/material';
+
+import HeroBannerImage from '../assets/images/banner.png';
 
 const HeroBanner = () => {
   return (
-    <div>HeroBanner</div>
+    <Box sx={{
+    mt: {lg: '212px', xs: '70px'},
+    ml: {sm: '50px'}
+  }}
+    position="relative"
+    p="20px"
+    >
+      <Typography color="#FF2625"
+      fontWeight="600" fontSize="26px">
+        Fitness Club
+      </Typography>
+      <Typography fontWeight="700"
+      sx={{fontSize: {lg: '44px', xs: '40px'}}}>
+        Sweat, Smile <br/> and repeat
+      </Typography>
+      <Typography fontSize="22px" LineHeight="35px" mb={3}>
+        Check out the most effective ecetcises
+      </Typography>
+      <Button variant="contained" color="error" href="#exercises">Explore Exercises</Button>
+      <img src={HeroBannerImage} alt="banner" className="hero-banner-img"/>
+    </Box>
   )
 }
 
