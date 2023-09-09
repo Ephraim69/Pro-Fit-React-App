4bf5f2e11b76a2de735542163d5976cca1db87a8
ephraim69 <ephraimigbinosa@gmail.com>
2023-09-09 11:51:57 +0100

    complete hero banner and begin searchComponent

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -17,13 +17,27 @@ const HeroBanner = () => {
         Fitness Club
       </Typography>
       <Typography fontWeight="700"
-      sx={{fontSize: {lg: '44px', xs: '40px'}}}>
+      sx={{fontSize: {lg: '44px', xs: '40px'}}}
+      mb="23px"
+      mt="30px">
         Sweat, Smile <br/> and repeat
       </Typography>
-      <Typography fontSize="22px" LineHeight="35px" mb={3}>
-        Check out the most effective ecetcises
+      <Typography fontSize="22px" LineHeight="35px" mb={4}>
+        Check out the most effective exercises
       </Typography>
-      <Button variant="contained" color="error" href="#exercises">Explore Exercises</Button>
+      <Button variant="contained" color="error" href="#exercises"
+      sx={{backgroundColor: "#ff2625", padding: '10px'}}
+      >Explore Exercises</Button>
+      <Typography 
+      fontWeight={600}
+      color="#FF2625"
+      sx={{
+        opacity: 0.1,
+        display: {lg: 'block', xs: 'none'}
+      }}
+      fontSize="200px">
+        Exercises
+      </Typography>
       <img src={HeroBannerImage} alt="banner" className="hero-banner-img"/>
     </Box>
   )
