f1c79c3c425def21c6c6ceff5b3096cbf55be05d
ephraim69 <ephraimigbinosa@gmail.com>
2023-09-12 14:19:32 +0100

    fetch data

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,12 +1,16 @@
 import React, { useEffect, useState } from 'react'
 import { Box, Button, Stack, TextField, Typography } from '@mui/material'
 
+import {exerciseOptions, fetchData} from '../utils/fetchData';
+
 const SearchExercises = () => {
   const [search, setSearch] = useState('');
 
   const handleSearch = async () => {
     if(search) {
-      const exercisesData = await fetchData();
+      const exercisesData = await fetchData(
+        'https://exercisedb.p.rapidapi.com/exercises/bodyPartList', exerciseOptions);
+      console.log(exercisesData);
     }
   }
 
@@ -45,7 +49,7 @@ const SearchExercises = () => {
           position: "absolute",
           right: 0
         }}
-        onCLick={handleSearch}
+        onClick={handleSearch}
         >
           Search
         </Button>
