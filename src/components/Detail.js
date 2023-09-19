b2e582c6f396dca3d5145ba8a463567b77d414ca
ephraim69 <ephraimigbinosa@gmail.com>
2023-09-19 13:24:37 +0100

    start implementing details component

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,7 +1,25 @@
 import React from "react";
+import { Typography, Stack, Button } from "@mui/material";
 
-const Detail = () => {
-  return <div>Detail</div>;
+import BodyPartImage from "../assets/icons/body-part.png";
+import TargetImage from "../assets/icons/target.png";
+import EquipmentImage from "../assets/icons/equipment.png";
+
+const Detail = ({ exerciseDetail }) => {
+  const { bodyPart, equipment, gifUrl, name, target } = exerciseDetail;
+
+  return (
+    <Stack
+      gap="60px"
+      sx={{ flexDirection: { lg: "row" }, p: "20px", alignItems: "center" }}
+    >
+      <img src={gifUrl} alt={name} loading="lazy" className="detail-image" />
+      <Stack sx={{ gap: { lg: "35px", xs: "20px" } }}>
+        <Typography>{name}</Typography>
+        <Typography></Typography>
+      </Stack>
+    </Stack>
+  );
 };
 
 export default Detail;
