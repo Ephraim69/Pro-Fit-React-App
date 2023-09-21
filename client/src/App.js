e8c5fa89a884e51b06158000692ce2e30b62c6b5
ephraim69 <ephraimigbinosa@gmail.com>
2023-09-21 17:45:38 +0100

    begin implementing registration page

---
+++ client/src/App.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { Box } from "@mui/material";
+
+import "./App.css";
+import ExerciseDetail from "./pages/ExerciseDetail";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Register from "./pages/Register";
+
+const App = () => {
+  let loggedIn = false;
+
+  return (
+    <Box width={{ xl: "1488px" }} m="auto">
+      {!loggedIn ? (
+        <Routes>
+          <Route path="/" element={<Register />} />
+        </Routes>
+      ) : (
+        <>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/exercise/:id" element={<ExerciseDetail />} />
+          </Routes>
+        </>
+      )}
+      <Footer />
+    </Box>
+  );
+};
+
+export default App;
